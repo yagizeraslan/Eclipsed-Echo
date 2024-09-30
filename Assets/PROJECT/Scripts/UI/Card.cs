fe4fa9df32495dee672d4296734fc977b06b3d26
yagizeraslan <yagizeraslan@gmail.com>
2024-09-30 03:24:31 +0300

    Card.cs match-mismatch-flip-hide logics added

--- Assets/PROJECT/Scripts/UI/Card.cs
+++ Assets/PROJECT/Scripts/UI/Card.cs
@@ -1,28 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 
-public class Card : MonoBehaviour, IPointerClickHandler
+
+namespace YagizEraslan.EclipsedEcho
 {
-    [SerializeField] private Image cardFaceImg;
-    [SerializeField] private Image cardBackImg;
-
-    public void OnPointerClick(PointerEventData eventData)
+    [RequireComponent(typeof(Animator))]
+    public class Card : MonoBehaviour, IPointerClickHandler
     {
-        throw new System.NotImplementedException();
-    }
+        public int CardID { get; private set; }
+        public bool IsMatched { get; private set; }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        [SerializeField] private Image frontImage;
+        [SerializeField] private Image backImage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public async Task Initialize(int cardID, string frontSpriteAddress, string backSpriteAddress)
+        {
+            CardID = cardID;
+
+            // Load front sprite
+            var frontSpriteLoad = Addressables.LoadAssetAsync<Sprite>(frontSpriteAddress);
+            await frontSpriteLoad.Task;
+            frontImage.sprite = frontSpriteLoad.Result;
+
+            // Load back sprite
+            var backSpriteLoad = Addressables.LoadAssetAsync<Sprite>(backSpriteAddress);
+            await backSpriteLoad.Task;
+            backImage.sprite = backSpriteLoad.Result;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (IsMatched || GameController.Instance.IsProcessing)
+                return;
+
+            FlipCard();
+            GameController.Instance.CardSelected(this);
+        }
+
+        public void FlipCard()
+        {
+            animator.SetTrigger("Flip");
+            SoundManager.Instance.PlayFlipSound();
+        }
+
+        public void Match()
+        {
+            IsMatched = true;
+            animator.SetTrigger("Match");
+            SoundManager.Instance.PlayMatchSound();
+        }
+
+        public void Mismatch()
+        {
+            animator.SetTrigger("Mismatch");
+            SoundManager.Instance.PlayMismatchSound();
+        }
+
+        public void HideCard()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
